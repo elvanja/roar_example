9e73d9ac1ae28fcb7c1306e8f9e9be19c59c3f9f
Nick Sutterer <apotonick@gmail.com>
2012-06-28 09:36:21 +0200

    add representer configuration.

--- tutti_frutti/config/environments/test.rb
+++ tutti_frutti/config/environments/test.rb
@@ -32,4 +32,6 @@ TuttiFrutti::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  config.representer.default_url_options = {:host => "fru.it"}
 end
