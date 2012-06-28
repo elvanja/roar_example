775bca1976bca5870f49e11ba2d2547251b66df9
Nick Sutterer <apotonick@gmail.com>
2012-06-28 09:36:45 +0200

    require the gem'ed representers here manually.

--- tutti_frutti/app/controllers/fruits_controller.rb
+++ tutti_frutti/app/controllers/fruits_controller.rb
@@ -1,3 +1,7 @@
+# FIXME: if required earlier, the url_helpers is not complete and thus representers don't work correctly. see here: https://groups.google.com/forum/?fromgroups#!topic/rubyonrails-core/5tG5unZ8jDQ
+require "book_of_orcharding/representers/fruit_representer"
+require "book_of_orcharding/representers/fruits_representer"
+
 class FruitsController < ApplicationController
   include Roar::Rails::ControllerAdditions
   represents :json, Orcharding::Fruit
