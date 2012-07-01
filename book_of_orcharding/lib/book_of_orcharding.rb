7f18d78bb508dccdfb6be92c01a497868cffe6ad
Nick Sutterer <apotonick@gmail.com>
2012-07-01 10:54:24 +0200

    move domain models to orchard since we don't need it in the client.

--- book_of_orcharding/lib/book_of_orcharding.rb
+++ book_of_orcharding/lib/book_of_orcharding.rb
@@ -1,7 +1,7 @@
 require "book_of_orcharding/version"
 
 root = File.expand_path('..', __FILE__)
-%w{util domain persistence}.each do |dir|
+%w{util}.each do |dir|
   Dir["#{root}/book_of_orcharding/#{dir}/**/*.rb"].sort.each { |source| require source }
 end
 
