04427b867ad6b3e6fb9491957355866ce33d3fd2
Nick Sutterer <apotonick@gmail.com>
2012-07-01 11:10:40 +0200

    use url instead of id for Fruit in index.

--- smoothie_mixer/app/controllers/fruits_controller.rb
+++ smoothie_mixer/app/controllers/fruits_controller.rb
@@ -5,10 +5,10 @@ class FruitsController < ApplicationController
   include Roar::Rails::ControllerAdditions
 
   def index
-    @fruits = ::Fruits.get("http://localhost:9292/fruits", 'application/json').fruits
+    @fruits = Fruits.get("http://localhost:9292/fruits", 'application/json').fruits
   end
 
   def edit
-    @fruit = ::Fruit.get(params[:id], 'application/json')
+    @fruit = Fruit.get(params[:id], 'application/json')
   end
 end
